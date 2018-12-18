f3fd94241873a405e372e706b8202c4e39e9a102
Alexander Kluth <deralex@cpan.org>
2018-12-18 21:36:27 +0100

    Initial commit
    Signed-off-by: Alexander Kluth <deralex@cpan.org>

---
+++ cheesy.c
@@ -0,0 +1,39 @@
+#include <vte/vte.h>
+
+int main(int argc, char *argv[])
+{
+    GtkWidget *window, *terminal;
+    PangoFontDescription *pfd;
+
+    /* Initialise GTK, the window and the terminal */
+    gtk_init(&argc, &argv);
+    terminal = vte_terminal_new();
+    window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    gtk_window_set_title(GTK_WINDOW(window), "cheesy");
+
+    /* Start a new shell */
+    gchar **envp = g_get_environ();
+    gchar **command = (gchar *[]){g_strdup(g_environ_getenv(envp, "SHELL")), NULL};
+    g_strfreev(envp);
+    vte_terminal_spawn_async(VTE_TERMINAL(terminal),
+        VTE_PTY_DEFAULT,
+        NULL,       /* working directory  */
+        command,    /* command */
+        NULL,       /* environment */
+        0,          /* spawn flags */
+        NULL, NULL, /* child setup */
+        NULL,       /* child pid */
+        -1, NULL, NULL, NULL);
+
+    pfd = pango_font_description_from_string("Source Code Pro Medium");
+    vte_terminal_set_font(VTE_TERMINAL(terminal), pfd);
+
+    /* Connect some signals */
+    g_signal_connect(window, "delete-event", gtk_main_quit, NULL);
+    g_signal_connect(terminal, "child-exited", gtk_main_quit, NULL);
+
+    /* Put widgets together and run the main loop */
+    gtk_container_add(GTK_CONTAINER(window), terminal);
+    gtk_widget_show_all(window);
+    gtk_main();
+}
