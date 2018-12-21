2090e150b26005458a07ffb9d5daa01d689a5738
Alexander Kluth <deralex@cpan.org>
2018-12-21 18:16:41 +0100

    Add first version of autotools
    Signed-off-by: Alexander Kluth <deralex@cpan.org>

---
+++ config.h
@@ -0,0 +1,38 @@
+/* config.h.  Generated from config.h.in by configure.  */
+/* config.h.in.  Generated from configure.ac by autoheader.  */
+
+/* Define to 1 if you have the `cairo' library (-lcairo). */
+#define HAVE_LIBCAIRO 1
+
+/* Define to 1 if you have the `gdk-3' library (-lgdk-3). */
+#define HAVE_LIBGDK_3 1
+
+/* Define to 1 if you have the `gnutls' library (-lgnutls). */
+#define HAVE_LIBGNUTLS 1
+
+/* Define to 1 if you have the `gtk-3' library (-lgtk-3). */
+#define HAVE_LIBGTK_3 1
+
+/* Define to 1 if you have the `vte-2.91' library (-lvte-2.91). */
+#define HAVE_LIBVTE_2_91 1
+
+/* Define to 1 if you have the `z' library (-lz). */
+#define HAVE_LIBZ 1
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "BUG-REPORT-ADDRESS"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "FULL-PACKAGE-NAME"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "FULL-PACKAGE-NAME VERSION"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "full-package-name"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL ""
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "VERSION"
